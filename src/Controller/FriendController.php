259c9266e324dc641d4ed693398d3cd1c6f807b9
Denis Klementjev <dklementjev@yandex.ru>
2024-09-11 00:41:32 +0300

    UserFriend model and controller are added

---
+++ src/Controller/FriendController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\User;
+use App\Utils\Model\UserFriend;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(name:'friend_', path: '/friend')]
+class FriendController
+{
+    public function __construct(
+        protected readonly Security $security,
+        protected readonly UserFriend $userFriendUtils,
+    ) {}
+
+    #[Route(name: 'add', path: '/set/{friend_id}', methods: ['PUT'], requirements: ['friend_id' => '\d+'])]
+    public function addAction(Request $request)
+    {
+        /** @var User */
+        $user = $this->security->getUser();
+        $userId = $user->getId();
+        $friendId = $request->attributes->getInt("friend_id");
+
+        $userFriend = $this->userFriendUtils->getByUserIdAndFriendId($userId, $friendId);
+        if ($userFriend === null) {
+            $this->userFriendUtils->addFriendById($userId, $friendId);
+        }
+
+        return new JsonResponse([
+            'success' => true,
+        ]);
+    }
+
+    #[Route(name: 'delete', path: '/delete/{friend_id}', methods: ['PUT'], requirements: ['friend_id' => '\d+'])]
+    public function deleteAction(Request $request)
+    {
+        /** @var User */
+        $user = $this->security->getUser();
+        $userId = $user->getId();
+        $friendId = $request->attributes->getInt("friend_id");
+
+
+        $this->userFriendUtils->deleteByUserIdAndFriendId($userId, $friendId);
+
+        return new JsonResponse([
+            'success' => true,
+        ]);
+    }
+}
---
+++ src/Repository/UserFriendRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\UserFriend;
+use Doctrine\DBAL\Connection;
+
+class UserFriendRepository
+{
+    protected Connection $rwConnection;
+
+    protected Connection $roConnection;
+
+    public function __construct(
+        Connection $dbConnection,
+        Connection $slaveConnection
+    ) {
+        $this->rwConnection = $dbConnection;
+        $this->roConnection = $slaveConnection;
+    }
+
+    public function getByUserIdAndFriendId(int $userId, int $friedndId): ?UserFriend
+    {
+        $sql = <<<'SQL'
+        SELECT uf.*
+            FROM app_user_friends AS uf
+            WHERE user_id=:user_id AND friend_id=:friend_id
+SQL;
+        return $this->hydrate(
+            $this->roConnection->fetchAssociative(
+                $sql,
+                ['user_id' => $userId, 'friend_id' => $friedndId]
+            )
+        );
+    }
+
+    public function deleteByUserIdAndFriendId(int $userId, int $friedndId): int
+    {
+        $sql = <<<'SQL'
+        DELETE FROM app_user_friends AS uf
+            WHERE user_id=:user_id AND friend_id=:friend_id
+SQL;
+        return $this->rwConnection->executeStatement(
+            $sql,
+            [
+                'user_id' => $userId,
+                'friend_id' => $friedndId,
+            ]
+            );
+    }
+
+    public function addFriendId(int $userId, int $friendId): int
+    {
+        $sql = <<<'SQL'
+        INSERT INTO app_user_friends(user_id, friend_id)
+            VALUES (:user_id, :friend_id)
+            ON CONFLICT DO NOTHING
+SQL;
+        return $this->rwConnection->executeStatement(
+            $sql,
+            [
+                'user_id' => $userId,
+                'friend_id' => $friendId
+            ]
+        );
+    }
+
+    protected function hydrate(array|bool $rawData): ?UserFriend
+    {
+        if ($rawData === false) {
+            return null;
+        }
+
+        return new UserFriend(
+            $rawData['user_id'],
+            $rawData['friend_id']
+        );
+    }
+}
