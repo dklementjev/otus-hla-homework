fedc36fa254f5817143a1cb97e9e346b3a531669
Denis Klementjev <dklementjev@yandex.ru>
2024-09-14 03:20:48 +0300

    UserProvider: phpdoc updates, exception throwing on missing user

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -4,20 +4,30 @@ namespace App\Security;
 
 use App\Model\User;
 use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
+/**
+ * @template-implements UserProviderInterface<User>
+ */
 class UserProvider implements UserProviderInterface
-{    
+{
     public function __construct(
         protected readonly UserRepository $userRepository
     ) {}
 
     public function loadUserByIdentifier(string $identifier): UserInterface
     {
-        return $this->userRepository->getById((int) $identifier);
+        $res = $this->userRepository->getById((int) $identifier);
+
+        if ($res === null) {
+            throw new UserNotFoundException();
+        }
+
+        return $res;
     }
-    
+
     public function refreshUser(UserInterface $user): UserInterface
     {
         throw new \LogicException("Not implemented");
