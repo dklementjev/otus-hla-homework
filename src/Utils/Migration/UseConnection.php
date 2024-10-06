05a0bd76f48ca192e05393acd92ee9b01e0ffa61
Denis Klementjev <dklementjev@yandex.ru>
2024-10-07 01:26:12 +0300

    ForeignMigration is implemented, allowing foreign DB migrations while tracking version history in default one

---
+++ src/Utils/Migration/UseConnection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Utils\Migration;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class UseConnection
+{
+    public function __construct(
+        public readonly string $connection
+    ) {}
+}
