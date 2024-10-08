4b13a98fd6e0e8b06e0f5c5ed20bf0eb37e1881d
Denis Klementjev <dklementjev@yandex.ru>
2024-10-08 21:41:46 +0300

    DB migrations: reference tables are set up

---
+++ migrations/Version20241008183351.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Utils\Migration\ForeignMigration;
+use App\Utils\Migration\UseConnection;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+
+#[UseConnection('dialog')]
+final class Version20241008183351 extends ForeignMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $referenceTableNames = [
+            'app_dialog_users',
+            'app_dialogs',
+            'app_dialog_participants',
+        ];
+
+        foreach ($referenceTableNames as $tableName) {
+            $this->createReferenceTable($tableName);
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration();
+    }
+
+    protected function createReferenceTable(string $tableName)
+    {
+        $sql = <<<'SQL'
+        SELECT create_reference_table(:table_name) 
+SQL;
+        $this->getConnection()->executeQuery($sql, ['table_name' => $tableName]);
+    }
+}
