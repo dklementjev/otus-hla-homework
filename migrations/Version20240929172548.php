77b55909d7090118eb521c4a7b487fc7ce78feb5
Denis Klementjev <dklementjev@yandex.ru>
2024-10-08 21:12:19 +0300

    DB migrations: app_dialog_users table is added

--- migrations/Version20240929172548.php
+++ migrations/Version20240929172548.php
@@ -20,6 +20,14 @@ final class Version20240929172548 extends ForeignMigration
     {
         $connection = $this->getConnection();
 
+        $sql = <<< 'SQL'
+        CREATE TABLE app_dialog_users (
+            id BIGINT PRIMARY KEY,
+            nickname VARCHAR(128)
+        )
+SQL;
+        $connection->executeQuery($sql);
+
         $sql = <<<'SQL'
         CREATE TABLE app_dialogs (
             id BIGSERIAL PRIMARY KEY, 
@@ -32,7 +40,7 @@ SQL;
         $sql = <<<'SQL'
         CREATE TABLE app_dialog_participants (
             dialog_id INT REFERENCES app_dialogs(id) ON DELETE CASCADE, 
-            user_id INT REFERENCES app_users(id) ON DELETE CASCADE,
+            user_id INT REFERENCES app_dialog_users(id) ON DELETE CASCADE,
             PRIMARY KEY (dialog_id, user_id)
         )
 SQL;
@@ -42,7 +50,7 @@ SQL;
         CREATE TABLE app_dialog_messages (
             id BIGSERIAL PRIMARY KEY,
             "uuid" UUID UNIQUE,
-            user_id INT REFERENCES app_users(id),
+            user_id INT REFERENCES app_dialog_users(id),
             dialog_id INT REFERENCES app_dialogs(id), 
             message TEXT NOT NULL, 
             created_at TIMESTAMP WITH TIME ZONE
@@ -65,5 +73,6 @@ SQL;
         $connection->executeQuery('DROP TABLE app_dialog_messages');
         $connection->executeQuery('DROP TABLE app_dialog_participants');
         $connection->executeQuery('DROP TABLE app_dialogs');
+        $connection->executeQuery('DROP TABLE app_dialog_users');
     }
 }
