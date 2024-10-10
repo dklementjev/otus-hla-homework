fed4495313fc4c627878ec6e9d5045388e07b58b
Denis Klementjev <dklementjev@yandex.ru>
2024-10-11 02:10:28 +0300

    RepositoryMapper is added

---
+++ src/Repository/ModelClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repository;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class ModelClass
+{
+    public function __construct(
+        public readonly string $name
+    ) {}
+}
