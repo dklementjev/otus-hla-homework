cd5a76f7b9b4949483bc31d1933aee19b5d7ba07
Denis Klementjev <dklementjev@yandex.ru>
2024-10-14 02:06:38 +0300

    Dialog entity utils class is prototyped

---
+++ src/Utils/Model/Dialog.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Utils\Model;
+
+use App\Model\Dialog as DialogModel;
+use App\Repository\DialogParticipantRepository;
+use App\Repository\DialogRepository;
+use App\Repository\DialogUserRepository;
+
+class Dialog
+{
+    public function __construct(
+        protected readonly DialogRepository $dialogRepository,
+        protected readonly DialogUserRepository $dialogUserRepository,
+        protected readonly DialogParticipantRepository $dialogParticipantRepository
+    ) {}
+
+    public function getPMForUsers(int $userId, int $otherUserId): ?DialogModel
+    {
+        return $this->dialogRepository->getPMForUsers($userId, $otherUserId);
+    }
+
+    public function createPMForUsers(int $userId, int $otherUserId): DialogModel
+    {
+        $user = $this->dialogUserRepository->getOrCreateByUserId($userId);
+        $otherUser = $this->dialogUserRepository->getOrCreateByUserId($otherUserId);
+        $dialog = $this->dialogRepository->insert($this->dialogRepository->create(false));
+        $this->dialogParticipantRepository->insert($this->dialogParticipantRepository->create($dialog->getId(), $user->getId()));
+        $this->dialogParticipantRepository->insert($this->dialogParticipantRepository->create($dialog->getId(), $otherUser->getId()));
+
+        return $dialog;
+    }
+
+    public function getOrCreatePMForUsers(int $userId, int $otherUserId): DialogModel
+    {
+        return $this->getPMForUsers($userId, $otherUserId) ?? $this->createPMForUsers($userId, $otherUserId);
+    }
+}
