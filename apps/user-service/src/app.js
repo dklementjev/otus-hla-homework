d7e9f05018751dc38c11d1c7b868dd6b48a5436a
Denis Klementjev <dklementjev@yandex.ru>
2024-12-25 01:20:43 +0300

    user-service: user search endpoint is added

--- apps/user-service/src/app.js
+++ apps/user-service/src/app.js
@@ -1,7 +1,7 @@
 /* global Deno */
 import Debug from "debug";
 import Koa from "koa";
-import { Client as PostgreClient} from "https://deno.land/x/postgres/mod.ts";
+import {Client as PostgreClient} from "https://deno.land/x/postgres/mod.ts";
 import R from "koa-route";
 
 const appDebug = Debug("app");
@@ -26,16 +26,62 @@ async function tokenAction(ctx, token) {
     }
 }
 
-const app = new Koa();
-const dbOptions = {
-    hostname: Deno.env.get('POSTGRESQL_HOST'),
-    port: Deno.env.get('POSTGRESQL_PORT'),
-    user: Deno.env.get('POSTGRESQL_USER'),
-    password: Deno.env.get('POSTGRESQL_PASSWORD'),
-    database: Deno.env.get('POSTGRESQL_DB'),
+async function userSearchAction(ctx) {
+    const firstName = ctx.request.query.first_name || null;
+    const lastName = ctx.request.query.last_name || null;
+
+    if (!firstName || !lastName) {
+        ctx.response.status = 404;
+        return;
+    }
+
+    const sth = await dbConnection.queryObject(
+        'SELECT * FROM app_users AS u WHERE starts_with(lower(u.first_name), $first_name) AND starts_with(lower(u.last_name), $last_name) ORDER BY u.id',
+        {
+            first_name: firstName.toLowerCase(),
+            last_name: lastName.toLowerCase()
+        }
+    );
+
+    httpDebug('userSearch for "%s" "%s": %s results', firstName, lastName, sth.rows.length);
+    const users = sth.rows.map(
+        (rawUser) => ({id: rawUser.id, first_name: rawUser.first_name, second_name: rawUser.last_name})
+    );
+
+    ctx.response.body = {items: users};
 }
 
-const dbConnection = new PostgreClient(dbOptions)
+const app = new Koa();
+
+function getDBOptions(debugSQL) {
+    const res = {
+        hostname: Deno.env.get('POSTGRESQL_HOST'),
+        port: Deno.env.get('POSTGRESQL_PORT'),
+        user: Deno.env.get('POSTGRESQL_USER'),
+        password: Deno.env.get('POSTGRESQL_PASSWORD'),
+        database: Deno.env.get('POSTGRESQL_DB'),
+    };
+
+    if (debugSQL) {
+        Object.assign(
+            res,
+            {
+                controls: {
+                    debug: {
+                        queries: true,
+                        notices: true,
+                        results: true,
+                        queryInError: true,
+                    }
+                }
+            }
+        )
+    }
+
+    return res;
+}
+
+const dbConnection = new PostgreClient(getDBOptions(false))
 app.use(async (ctx, next) => {
     const req = ctx.request;
     const res = ctx.response;
@@ -47,6 +93,7 @@ app.use(async (ctx, next) => {
     ctx.response.append('x-request-id', requestId);
 });
 app.use(R.get('/', pingAction))
+app.use(R.get('/user/search', userSearchAction));
 app.use(R.get('/token/:token', tokenAction));
 
 appDebug(`Connecting to DB`);
